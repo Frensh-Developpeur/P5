c65ba686e981fdd178889c75d2d6f0efdde3a4fe
Seb Dev <sebckyser727@gmail.com>
2022-02-17 00:04:18 +0100

    Page d'accueil fini ! Page produit finir à vérifier ( Application dans le localStorage) + Résolutions de failles potentiels  :+1:

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,3 @@
-
-
 let params = (new URL(window.location)).searchParams;
 let id = params.get('id');
 
@@ -13,8 +11,6 @@ fetch('http://localhost:3000/api/products/' + id)
         console.log(value)
         let tableColors = [];
         tableColors = value.colors;
-        console.log(tableColors)
-
         document.querySelector('.item__img').innerHTML += `<img src="${value.imageUrl}" alt="${value.altTxt}">`
         document.getElementById('title').innerHTML = `${value.name}`
         document.getElementById('price').innerHTML = `${value.price}`
@@ -23,6 +19,38 @@ fetch('http://localhost:3000/api/products/' + id)
         for (i = 0; i < tableColors.length; i++) {
             document.getElementById('colors').innerHTML += `<option>${tableColors[i]}</option> `
         }
+        let eventButton = document.getElementById('addToCart');
+
+        eventButton.addEventListener('click', function () {
+            let optionProduit = {
+                id: value._id,
+                name: value.name,
+                priceUnit: value.price,
+                quantiter: document.getElementById('quantity').value,
+                colorItem: document.getElementById('colors').value
+            }
+            let productStorage = JSON.parse(localStorage.getItem("product"));
+
+            if (optionProduit.quantiter <= 0 && optionProduit.quantiter === typeof (Number) || optionProduit.colorItem >= 0) {
+                alert("Vous avez mal renseigner une valeur ! Veuillez choisir une couleur et ainsi que le nombre d'article souhaitée");
+                return false;
+            } else {
+
+            }
+            if (productStorage) {
+                productStorage.push(optionProduit);
+                localStorage.setItem("product", JSON.stringify(productStorage));
+            }
+            else {
+                productStorage = [];
+                productStorage.push(optionProduit)
+                localStorage.setItem("product", JSON.stringify(productStorage));
+            }
+
+            console.log(productStorage);
+        })
+
+
 
     })
     .catch(function (err) {
